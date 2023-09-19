d4ae4fd31848be0be7ab35061a3fa993a15d6acb
Phạm Tuấn Giang <43736478+mrgiangoip@users.noreply.github.com>
2023-09-19 17:42:19 +0700

    Update md5, SignedCookie
    Update md5, SignedCookie

--- Dev/example-express/1-hello-coder-tokyo/index.js
+++ Dev/example-express/1-hello-coder-tokyo/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
 
 var userRoute = require('./routes/user.route');
+var authRoute = require('./routes/auth.route');
+
+var authMiddleware = require('./middlewares/auth.middleware');
 
 var port = 3000;
 
@@ -10,8 +14,9 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser('cutconmemaydi2023'));
 
 app.use(express.static('public'));
 
@@ -28,7 +33,8 @@ app.get('/', function(request, response){
 	});
 });
 
-app.use('/users', userRoute);
+app.use('/users', authMiddleware.requireAuth,userRoute);
+app.use('/auth', authRoute);
 
 app.listen(port, function(){
 	console.log('Server listenning on port' + port);
--- Dev/example-express/1-hello-coder-tokyo/routes/user.route.js
+++ Dev/example-express/1-hello-coder-tokyo/routes/user.route.js
@@ -1,11 +1,18 @@
 var	express = require('express');
 
-var controller = require('../controllers/user.controlle')
+var controller = require('../controllers/user.controller');
+var validate = require('../validate/user.validate');
+var authMiddleware = require('../middlewares/auth.middleware');
 
 var router = express.Router();
 
 
-router.get('/', controller.index);
+router.get('/', authMiddleware.requireAuth, controller.index);
+
+router.get('/cookie', function(request, response, next){
+ response.cookie('user-id', 12345);
+ response.send('hello');
+});
 
 router.get('/search', controller.search);
 
@@ -13,6 +20,6 @@ router.get('/create', controller.create);
 
 router.get('/:id', controller.get);
 
-router.post('/create', controller.postCreate);
+router.post('/create', validate.postCreate, controller.postCreate);
 
 module.exports = router;
