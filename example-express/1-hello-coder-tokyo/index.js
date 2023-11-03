84697eefd834e82b17840eeb65c9877dbc1a2b81
mrgiangoip <mr.giangoip@gmail.com>
2023-11-03 18:43:23 +0700

    8 product example-express session

--- example-express/1-hello-coder-tokyo/index.js
+++ example-express/1-hello-coder-tokyo/index.js
@@ -7,8 +7,10 @@ var cookieParser = require('cookie-parser')
 var userRoute = require('./routes/user.route');
 var authRoute = require('./routes/auth.route');
 var productRoute = require('./routes/product.route');
+var cartRoute = require('./routes/cart.route');
 
 var authMiddleware = require('./middlewares/auth.middleware');
+var sessionMiddleware = require('./middlewares/session.middleware');
 
 var port = 3000;
 
@@ -20,6 +22,7 @@ app.set('views', './views');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cookieParser(process.env.SESSION_SECRET));
+app.use(sessionMiddleware);
 
 app.use(express.static('public'));
 
@@ -39,6 +42,7 @@ app.get('/', function(request, response){
 app.use('/users', authMiddleware.requireAuth,userRoute);
 app.use('/auth', authRoute);
 app.use('/products', productRoute);
+app.use('/cart', cartRoute);
 
 app.listen(port, function(){
 	console.log('Server listenning on port' + port);
